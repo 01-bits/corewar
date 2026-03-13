ed6752a4b0d3f865f30bf3e0612a8abd5a271744
abdrahmanbalouri <balouriabdrahman@gmail.com>
2026-03-13 22:28:35 +0000

    add

--- common/src/constants.rs
+++ common/src/constants.rs
@@ -1,7 +1,7 @@
-const MEM_SIZE: u16 = 4096;
-const CHAMP_MAX_SIZE: u16 = 682;
-const REG_NUMBER: u8 = 16;
-const CYCLE_TO_DIE: u16 = 1536;
-const CYCLE_DELTA: u8 = 15;
-const NBR_LIVE: u8 = 21;
-const MAX_CHECKS: u8 = 10;
+pub const MEM_SIZE: u16 = 4096;
+pub const CHAMP_MAX_SIZE: u16 = 682;
+pub const REG_NUMBER: u8 = 16;
+pub const CYCLE_TO_DIE: u16 = 1536;
+pub const CYCLE_DELTA: u8 = 15;
+pub const NBR_LIVE: u8 = 21;
+pub const MAX_CHECKS: u8 = 10;
