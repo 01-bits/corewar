147caafb704f6c4e58b0bd3acd44dc09e9b2aeeb
abdrahmanbalouri <balouriabdrahman@gmail.com>
2026-03-14 06:37:53 +0000

    add

--- vm/src/loader.rs
+++ vm/src/loader.rs
@@ -3,4 +3,6 @@
 // hna ntikiw b constans  likayn f   pub const COREWAR_EXEC_MAGIC: u32 = 0x00ea83f3; // version of file .cor>>//
 
 //wach basah hada file .cor
- 
+ //let magic = read_u32_be(&file_bytes, 0); 
+
+ // if (magic != magic number)  is not .cor file
